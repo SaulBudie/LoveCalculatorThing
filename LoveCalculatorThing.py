001dcaa88297ea713c03d3d9efa672b025f9f879
Saul Budie <101558088+SaulBudie@users.noreply.github.com>
2022-04-29 00:06:33 +0700

    Add files via upload

---
+++ LoveCalculatorThing.py
@@ -0,0 +1,64 @@
+import random
+import time
+import cv2
+import sys
+sys.stdin
+def Intake_Gender():
+    c = input("What's your gender\n")
+    c2 = input("What's the other person's gender?\n")
+
+def WaitingScreen():
+    time.sleep(1)
+    print("Initiating Love Calculations")
+    time.sleep(0.2)
+    print("Please stand by")
+    time.sleep(1)
+    print("-------------------- 0%⠀done")
+    time.sleep(1)
+    print("**------------------ - 10%⠀done")
+    time.sleep(0.5)
+    print("******-------------- - 30%⠀done")
+    time.sleep(0.25)
+    print("**********---------- - 55%⠀done")
+    time.sleep(1)
+    print("**************------ - 75%⠀done")
+    time.sleep(0.75)
+    print("******************-- - 99%⠀done")
+    time.sleep(1)
+    print("******************** - 100%⠀done")
+def publishingRELATIONSHIP():
+    print("Printing...")
+def Run_Program():
+    print("Go to https://github.com/SaulBudie to support me writing shits.")
+    Intake_Gender()
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print("..")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    name1 = input("Please type your Name\n")
+    name2 = input("Please type your Crush's Name\n")
+    WaitingScreen()
+    time.sleep(0.5)
+    publishingRELATIONSHIP()
+    time.sleep(0.5)
+    print(f"The love chance between {name1} and {name2} is ...")
+    time.sleep(1)
+    FinalProductOfDeception()
+
+def FinalProductOfDeception():
+    namer = random.randrange(100)
+    print(f"{namer} %!!!")
+    print(f"Congrats and goodbye.")
+    
+while True:  
+    Run_Program()
+    
+    print("'Program will automatically close in 10 secs'")
+    time.sleep(10)
+    break
+
+    
+
